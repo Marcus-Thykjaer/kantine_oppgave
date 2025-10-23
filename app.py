872be425f7c85a95fbd8275a4b052e1e912b6401
Marcus-Thykjaer <marcus@thykjaer.dk>
2025-10-23 20:38:28 +0200

    lagt til styling på varer og meny og lister med info til varer, meny og kontakt

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data import KONTAKT_INFO, MENY_ITEMS, VARER_ITEMS
 
 app = Flask(__name__)
 
@@ -8,17 +9,17 @@ def index():
 
 @app.route('/kontakt')
 def kontakt():
-    return render_template('kontakt.html')
+    return render_template('kontakt.html', kontakter=KONTAKT_INFO)
 
 
 @app.route('/meny')
 def meny():
-    return render_template('meny.html')
+    return render_template('meny.html', meny=MENY_ITEMS)
 
 
 @app.route('/varer')
 def varer():
-    return render_template('varer.html')
+    return render_template('varer.html', varer=VARER_ITEMS)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,23 @@
+KONTAKT_INFO = [
+    {"navn": "Telefon", "info": "+47 456 456 45"},
+    {"navn": "Epost", "info": "kantine@akdemiet.no"}
+]
+
+MENY_ITEMS = [
+    {"navn": "Lasagne med salat", "pris": 85, "bilde": "/static/images/lasagne.jpg"},
+    {"navn": "Kyllingwok med ris", "pris": 90, "bilde": "/static/images/kyllingwok.jpg"},
+    {"navn": "Pasta Bolognese", "pris": 80, "bilde": "/static/images/pasta_bolognese.jpg"},
+    {"navn": "Kjøttboller med potetmos", "pris": 85, "bilde": "/static/images/kjottboller.jpg"},
+    {"navn": "Kyllingwrap", "pris": 65, "bilde": "/static/images/kyllingwrap.jpg"},
+]
+
+VARER_ITEMS = [
+    {"navn": "Toast", "pris": 35, "bilde": "/static/images/toast.jpg"},
+    {"navn": "Cola Zero", "pris": 30, "bilde": "/static/images/cola_zero.jpg"},
+    {"navn": "Baguette Kylling", "pris": 55, "bilde": "/static/images/baguette_kylling.jpg"},
+    {"navn": "Salatboks", "pris": 60, "bilde": "/static/images/salat.jpg"},
+    {"navn": "Yoghurt", "pris": 25, "bilde": "/static/images/yoghurt.jpg"},
+    {"navn": "Kaffe", "pris": 20, "bilde": "/static/images/kaffe.jpg"},
+    {"navn": "Smoothie Jordbær", "pris": 45, "bilde": "/static/images/smoothie_jordbaer.jpg"},
+    {"navn": "Muffins Sjokolade", "pris": 30, "bilde": "/static/images/muffins.jpg"}
+]
