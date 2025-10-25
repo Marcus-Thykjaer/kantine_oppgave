62570b3b4189a67d2c74ac5759c98e60e2007006
Marcus-Thykjaer <marcus@thykjaer.dk>
2025-10-25 22:22:32 +0200

    lagt til mer styling og kometarer og hjemme side

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
+# Importerer Flask for å lage webapplikasjonen og render_template for å rendere HTML maler
 from flask import Flask, render_template
+# Importerer data fra data.py
 from data import KONTAKT_INFO, MENY_ITEMS, VARER_ITEMS
 
+# Initialiserer en flask app
 app = Flask(__name__)
 
+# Definerer en rute for hovedsiden ('/')
 @app.route('/')
 def index():
+    # Viser index.html malen
     return render_template('index.html')
 
+# Definerer en rute for kontaktsiden /kontakt
 @app.route('/kontakt')
 def kontakt():
+    # Viser kontakt.html malen og sender med kontaktinformasjon
     return render_template('kontakt.html', kontakter=KONTAKT_INFO)
 
-
+# Definerer en rute for menysiden ('/meny')
 @app.route('/meny')
 def meny():
+    # Viser meny.html-malen og sender med meny-data
     return render_template('meny.html', meny=MENY_ITEMS)
 
-
+# Definerer en rute for varesiden ('/varer')
 @app.route('/varer')
 def varer():
+    # Viser varer.html malen og sender med vare data
     return render_template('varer.html', varer=VARER_ITEMS)
 
+# Kjører appen hvis skriptet blir kjørt direkte
 if __name__ == '__main__':
     app.run(debug=True)
--- data.py
+++ data.py
@@ -1,18 +1,44 @@
+# Definert en liste med kontaktinformasjon for kantinen.
+# Hver kontakt er en ordbok med navn, nummer, epost og rolle.
 KONTAKT_INFO = [
-    {"navn": "Telefon", "info": "+47 456 456 45"},
-    {"navn": "Epost", "info": "kantine@akdemiet.no"}
+    {"navn": "Marcus",
+     "nummer": "+47 456 456 45",
+     "epost": "kantine@akdemiet.no",
+     "rolle": "Innkjøper"},
+
+    {"navn": "Emma",
+     "nummer": "+47 912 345 67",
+     "epost": "emma.hansen@akademiet.no",
+     "rolle": "Kantineleder"},
+
+    {"navn": "Jonas",
+     "nummer": "+47 987 654 32",
+     "epost": "jonas.larsen@akademiet.no",
+     "rolle": "Assistent"},
+    
+    {"navn": "Sofie",
+    "nummer": "+47 401 223 89",
+    "epost": "sofie.nilsen@akademiet.no",
+    "rolle": "Kokk"
+    }
 ]
 
+
+#Definert en liste med menyelementer for hver dag i uken.
+#Hvert element er en ordbok med dag navn på retten, pris og bilde.
 MENY_ITEMS = [
-    {"navn": "Lasagne med salat", "pris": 85, "bilde": "/static/images/lasagne.jpg"},
-    {"navn": "Kyllingwok med ris", "pris": 90, "bilde": "/static/images/kyllingwok.jpg"},
-    {"navn": "Pasta Bolognese", "pris": 80, "bilde": "/static/images/pasta_bolognese.jpg"},
-    {"navn": "Kjøttboller med potetmos", "pris": 85, "bilde": "/static/images/kjottboller.jpg"},
-    {"navn": "Kyllingwrap", "pris": 65, "bilde": "/static/images/kyllingwrap.jpg"},
+    {"dag": "Mandag", "navn": "Lasagne med salat", "pris": 85, "bilde": "/static/images/Lasagne.jpg"},
+    {"dag": "Tirsdag", "navn": "Kyllingwok med ris", "pris": 90, "bilde": "/static/images/Kyllingwok.jpg"},
+    {"dag": "Onsdag", "navn": "Pasta Bolognese", "pris": 80, "bilde": "/static/images/pasta_bolognese.jpg"},
+    {"dag": "Torsdag", "navn": "Kjøttboller med potetmos", "pris": 85, "bilde": "/static/images/Kjottboller.jpg"},
+    {"dag": "Fredag", "navn": "Kyllingwrap", "pris": 65, "bilde": "/static/images/Kyllingwrap.jpg"},
+
 ]
 
+#definert en liste med varer som er i kantina
+# Hvert element er en ordbok med navn på varen, pris og bilde.
 VARER_ITEMS = [
-    {"navn": "Toast", "pris": 35, "bilde": "/static/images/toast.jpg"},
+    {"navn": "Toast", "pris": 35, "bilde": "/static/images/Toast.jpg"},
     {"navn": "Cola Zero", "pris": 30, "bilde": "/static/images/cola_zero.jpg"},
     {"navn": "Baguette Kylling", "pris": 55, "bilde": "/static/images/baguette_kylling.jpg"},
     {"navn": "Salatboks", "pris": 60, "bilde": "/static/images/salat.jpg"},
